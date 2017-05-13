772a488e41ac151b36e75c7c12f0805b3ee25ded
hlk <hlk_1135@outlook.com>
2017-05-13 19:24:22 +0800

    商品的操作

--- src/main/java/com/ldu/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ldu.service;
-
-import com.ldu.pojo.User;
-
-public interface UserService {
-    public void addUser(User user);
-    public User getUserByPhone(String phone);
-    public void updateUserName(User user);
-}
